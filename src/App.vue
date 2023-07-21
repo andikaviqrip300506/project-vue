b459f4ccd1d5c2a2a5344e982dd10e4b2d15969d
andikaviqrip300506 <yelisalista0863@gmail.com>
2023-07-21 14:35:33 +0700

    project-vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,84 @@
-<script setup>
-import { ref } from 'vue'
-
-const msg = "<h1> Hai </h1>" // Harus memakai rawHTML
-const hai = "hai" // Bisa menggunakan {{ hai }}
-const link = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fepaper.mediaindonesia.com%2Fdetail%2Fmonyet-juga-menyapa-seperti-manusia&psig=AOvVaw10nWkOSJ_HI6_E-BxvRJgt&ust=1689922775160000&source=images&cd=vfe&opi=89978449&ved=0CA4QjRxqFwoTCODplo_bnIADFQAAAAAdAAAAABAI"
-const number = prompt("")
-const objek = {
-  id : 'container',
-  class : 'wrapper'
-}
-</script>
 
 <template>
-  <div :="objek">
-   <h1 v-html="msg"></h1> 
-   <h1> {{ hai }} </h1>
-   <a :href="link"> Klik </a>
-   <button :disabled="IsButtonDisabled"><a :href="link">Button</a></button>
-   <p v-if="seen"></p>
-   <p> {{ parseInt(number) }} </p>
-   <p> {{ number ? 'TRUE : 1' : 'FALSE : 0' }}</p>
-
-   
+  <div>
+    <h1>Formulir</h1>
+    <form @submit.prevent="submitorm">
+      <label for="">Nama</label>
+      <input class="{'is-invalid' : isInvalidName}" 
+      type="text" name="nama" id="nama" v-model="kontak.nama">
+      <div v-if = "isInvalidName" class="error-message">
+      Nama Harus Di Isi !
+    </div>
+      <br>
+      <label for="">Email</label>
+      <input class="{'is-invalid' : isInvalidEmail}" 
+      type="email" name="email" id="email" v-model="kontak.email">
+      <div v-if = "isInvalidEmail" class="error-message">
+       Email Harus Di Isi !
+       </div>
+      <br>
+      <button type="submit">Simpan</button>
+      <br>
+    </form>
+    <div v-if="showResult">
+      nama : {{ name }} <br>
+      email : {{ email }}
+    </div>
   </div>
 </template>
+
+<script>
+  
+import { ref } from 'vue';
+export default {
+  name: 'MyComponent',
+  data() {
+    return {
+       kontak : 
+        {
+          nama: '', 
+          email: ''
+        },
+      showResult: ref(false),
+      name: ref(''),
+      email: ref(''),
+      };
+    },
+    computed : {
+      isInvalidName() {
+        return this.kontak.nama.length < 1 ;
+      },
+      isInvalidEmail() {
+        return this.kontak.email.length < 1 ;
+      }
+    },
+      methods: {
+        submitorm() {
+      this.name = this.kontak.nama; 
+      this.email = this.kontak.email;
+      this.showResult = true;
+      this.isInvalidName = false;
+      this.isInvalidEmail = false
+         
+      }
+     }
+    }
+  
+     
+     
+</script>
+
+<style>
+h1 {
+  color: blue;
+}
+
+.error-message{
+  color:red;
+  margin-top: 5px
+}
+
+.is-invalid {
+  border-color:red;
+}
+</style>
